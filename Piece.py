5ea4be678bfabc360d58426e87ba9136ac836ce4
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-10 21:56:45 +0900

    Added Piece.to_char() function and its unit test

--- Piece.py
+++ Piece.py
@@ -33,6 +33,29 @@ class Piece(Enum):
         """
         return Color.BLACK if piece in {Piece.BP, Piece.BN, Piece.BB, Piece.BR, Piece.BQ, Piece.BK} else Color.WHITE
 
+    @staticmethod
+    def to_char(piece):
+        """
+        Returns the character notation of the given piece
+        :param piece: the piece to return the character notation of
+        :return: the character notation of the given piece
+        """
+        # TODO validate piece or add default value for dictionary?
+        return {
+            Piece.WP: 'P',
+            Piece.WN: 'N',
+            Piece.WB: 'B',
+            Piece.WR: 'R',
+            Piece.WQ: 'Q',
+            Piece.WK: 'K',
+            Piece.BP: 'p',
+            Piece.BN: 'n',
+            Piece.BB: 'b',
+            Piece.BR: 'r',
+            Piece.BQ: 'q',
+            Piece.BK: 'k',
+        }[piece]
+
 
 # only runs when this module is called directly
 if __name__ == '__main__':
@@ -48,3 +71,16 @@ if __name__ == '__main__':
     print(Piece.BR)
     print(Piece.BQ)
     print(Piece.BK)
+
+    print(Piece.to_char(Piece.WP))
+    print(Piece.to_char(Piece.WN))
+    print(Piece.to_char(Piece.WB))
+    print(Piece.to_char(Piece.WR))
+    print(Piece.to_char(Piece.WQ))
+    print(Piece.to_char(Piece.WK))
+    print(Piece.to_char(Piece.BP))
+    print(Piece.to_char(Piece.BN))
+    print(Piece.to_char(Piece.BB))
+    print(Piece.to_char(Piece.BR))
+    print(Piece.to_char(Piece.BQ))
+    print(Piece.to_char(Piece.BK))
--- tests/test_Piece.py
+++ tests/test_Piece.py
@@ -28,3 +28,21 @@ class TestPieceEnum(unittest.TestCase):
         self.assertEqual(Piece.color(Piece.BR), Color.BLACK)
         self.assertEqual(Piece.color(Piece.BQ), Color.BLACK)
         self.assertEqual(Piece.color(Piece.BK), Color.BLACK)
+
+    def test_to_char(self):
+        """
+        Tests the to_char() function of the Piece enum.
+        """
+
+        self.assertEqual(Piece.to_char(Piece.WP), 'P')
+        self.assertEqual(Piece.to_char(Piece.WN), 'N')
+        self.assertEqual(Piece.to_char(Piece.WB), 'B')
+        self.assertEqual(Piece.to_char(Piece.WR), 'R')
+        self.assertEqual(Piece.to_char(Piece.WQ), 'Q')
+        self.assertEqual(Piece.to_char(Piece.WK), 'K')
+        self.assertEqual(Piece.to_char(Piece.BP), 'p')
+        self.assertEqual(Piece.to_char(Piece.BN), 'n')
+        self.assertEqual(Piece.to_char(Piece.BB), 'b')
+        self.assertEqual(Piece.to_char(Piece.BR), 'r')
+        self.assertEqual(Piece.to_char(Piece.BQ), 'q')
+        self.assertEqual(Piece.to_char(Piece.BK), 'k')
