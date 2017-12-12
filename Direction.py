97b04f5fa60e7eb3025840b78a4ab6ec8a090ade
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-12 14:34:49 +0900

    Added a simple Direction enum without unit tests

---
+++ Direction.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This file defines the Direction enum.
+"""
+from enum import Enum
+
+
+class Direction(Enum):
+    """
+    This enum contains values for each direction.
+    """
+
+    U = 1
+    UR = 2
+    R = 3
+    DR = 4
+    D = 5
+    DL = 6
+    L = 7
+    UL = 8
+
+# only runs when this module is called directly
+if __name__ == '__main__':
+    for dir in Direction:
+        print(dir)
