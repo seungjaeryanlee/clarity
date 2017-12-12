2d7f18eb8e6fcc76d652245c67b1109ac64966f0
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-12 14:38:34 +0900

    Modified structure of ATTACK constant with the new Direction enum

--- constants.py
+++ constants.py
@@ -2,6 +2,7 @@ r"""
 This file defines the constants used in Clarity.
 """
 from BitBoard import BitBoard
+from Direction import Direction
 from Piece import Piece
 
 
@@ -10,30 +11,46 @@ from Piece import Piece
 ATTACK_WP = [BitBoard(0)] * 64
 ATTACK_BP = [BitBoard(0)] * 64
 
-ATTACK_N_BN = [BitBoard(0)] * 64
+ATTACK_N = [BitBoard(0)] * 64
 
 ATTACK_B_UL = [BitBoard(0)] * 64
 ATTACK_B_UR = [BitBoard(0)] * 64
 ATTACK_B_DL = [BitBoard(0)] * 64
 ATTACK_B_DR = [BitBoard(0)] * 64
 
+ATTACK_B = {
+    Direction.UL: ATTACK_B_UL,
+    Direction.UR: ATTACK_B_UR,
+    Direction.DL: ATTACK_B_DL,
+    Direction.DR: ATTACK_B_DR,
+}
+
 ATTACK_R_U = [BitBoard(0)] * 64
 ATTACK_R_D = [BitBoard(0)] * 64
 ATTACK_R_L = [BitBoard(0)] * 64
 ATTACK_R_R = [BitBoard(0)] * 64
+
+ATTACK_R = {
+    Direction.U: ATTACK_R_U,
+    Direction.D: ATTACK_R_D,
+    Direction.L: ATTACK_R_L,
+    Direction.R: ATTACK_R_R,
+}
 
 ATTACK_K = [BitBoard(0)] * 64
 
 # TODO create Direction enum for bishop, rook, and queen
 ATTACK = {
     Piece.WP: ATTACK_WP,
-    Piece.WN: ATTACK_N_BN,
-    Piece.WB: [ATTACK_B_UL, ATTACK_B_UR, ATTACK_B_DL, ATTACK_B_DR],
-    Piece.WR: [ATTACK_R_U, ATTACK_R_D, ATTACK_R_L,  ATTACK_R_R],
+    Piece.WN: ATTACK_N,
+    Piece.WB: ATTACK_B,
+    Piece.WR: ATTACK_R,
+    # Piece.WQ should not be needed since we can use N and R
     Piece.WK: ATTACK_K,
     Piece.BP: ATTACK_BP,
-    Piece.BN: ATTACK_N_BN,
-    Piece.BB: [ATTACK_B_UL, ATTACK_B_UR, ATTACK_B_DL, ATTACK_B_DR],
-    Piece.BR: [ATTACK_R_U, ATTACK_R_D, ATTACK_R_L, ATTACK_R_R],
+    Piece.BN: ATTACK_N,
+    Piece.BB: ATTACK_B,
+    Piece.BR: ATTACK_R,
+    # Piece.BQ should not be needed since we can use N and R
     Piece.BK: ATTACK_K,
 }
