02df7b600509052119831564eb821029a1ff86e6
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-12 14:48:50 +0900

    Added underscore prefix for 'private' ATTACK bitboards

--- constants.py
+++ constants.py
@@ -3,54 +3,55 @@ This file defines the constants used in Clarity.
 """
 from BitBoard import BitBoard
 from Direction import Direction
+from MoveType import MoveType
 from Piece import Piece
 
 
 # Attack bitboards
 # TODO fill data
-ATTACK_WP = [BitBoard(0)] * 64
-ATTACK_BP = [BitBoard(0)] * 64
+_ATTACK_WP = [BitBoard(0)] * 64
+_ATTACK_BP = [BitBoard(0)] * 64
 
-ATTACK_N = [BitBoard(0)] * 64
+_ATTACK_N = [BitBoard(0)] * 64
 
-ATTACK_B_UL = [BitBoard(0)] * 64
-ATTACK_B_UR = [BitBoard(0)] * 64
-ATTACK_B_DL = [BitBoard(0)] * 64
-ATTACK_B_DR = [BitBoard(0)] * 64
+_ATTACK_B_UL = [BitBoard(0)] * 64
+_ATTACK_B_UR = [BitBoard(0)] * 64
+_ATTACK_B_DL = [BitBoard(0)] * 64
+_ATTACK_B_DR = [BitBoard(0)] * 64
 
-ATTACK_B = {
-    Direction.UL: ATTACK_B_UL,
-    Direction.UR: ATTACK_B_UR,
-    Direction.DL: ATTACK_B_DL,
-    Direction.DR: ATTACK_B_DR,
+_ATTACK_B = {
+    Direction.UL: _ATTACK_B_UL,
+    Direction.UR: _ATTACK_B_UR,
+    Direction.DL: _ATTACK_B_DL,
+    Direction.DR: _ATTACK_B_DR,
 }
 
-ATTACK_R_U = [BitBoard(0)] * 64
-ATTACK_R_D = [BitBoard(0)] * 64
-ATTACK_R_L = [BitBoard(0)] * 64
-ATTACK_R_R = [BitBoard(0)] * 64
+_ATTACK_R_U = [BitBoard(0)] * 64
+_ATTACK_R_D = [BitBoard(0)] * 64
+_ATTACK_R_L = [BitBoard(0)] * 64
+_ATTACK_R_R = [BitBoard(0)] * 64
 
-ATTACK_R = {
-    Direction.U: ATTACK_R_U,
-    Direction.D: ATTACK_R_D,
-    Direction.L: ATTACK_R_L,
-    Direction.R: ATTACK_R_R,
+_ATTACK_R = {
+    Direction.U: _ATTACK_R_U,
+    Direction.D: _ATTACK_R_D,
+    Direction.L: _ATTACK_R_L,
+    Direction.R: _ATTACK_R_R,
 }
 
-ATTACK_K = [BitBoard(0)] * 64
+_ATTACK_K = [BitBoard(0)] * 64
 
 # TODO create Direction enum for bishop, rook, and queen
 ATTACK = {
-    Piece.WP: ATTACK_WP,
-    Piece.WN: ATTACK_N,
-    Piece.WB: ATTACK_B,
-    Piece.WR: ATTACK_R,
+    Piece.WP: _ATTACK_WP,
+    Piece.WN: _ATTACK_N,
+    Piece.WB: _ATTACK_B,
+    Piece.WR: _ATTACK_R,
     # Piece.WQ should not be needed since we can use N and R
-    Piece.WK: ATTACK_K,
-    Piece.BP: ATTACK_BP,
-    Piece.BN: ATTACK_N,
-    Piece.BB: ATTACK_B,
-    Piece.BR: ATTACK_R,
+    Piece.WK: _ATTACK_K,
+    Piece.BP: _ATTACK_BP,
+    Piece.BN: _ATTACK_N,
+    Piece.BB: _ATTACK_B,
+    Piece.BR: _ATTACK_R,
     # Piece.BQ should not be needed since we can use N and R
-    Piece.BK: ATTACK_K,
+    Piece.BK: _ATTACK_K,
 }
