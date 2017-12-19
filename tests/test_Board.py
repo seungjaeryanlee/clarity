103917c22b5099a034922e00bee59f97dcf321ac
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-19 21:54:52 +0900

    Fixed error in unit test for Board.move_gen()

--- tests/test_Board.py
+++ tests/test_Board.py
@@ -433,7 +433,7 @@ class TestBoardClass(unittest.TestCase):
 
         board.fen('r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10')
         moves = board.move_gen()
-        self.assertEqual(len(moves), 1)
+        self.assertEqual(len(moves), 46)
 
     def test_get_target_noncapture_moves(self):
         """
