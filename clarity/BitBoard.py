7700bde34501f2e9296aa6c5d6233694857b6fac
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-20 16:27:39 +0900

    Restructured docstrings in BitBoard.py

--- clarity/BitBoard.py
+++ clarity/BitBoard.py
@@ -11,68 +11,94 @@ class BitBoard:
     This class is used to encapsulate a 64-bit integer to use as a chessboard of bits.
     """
 
-    def __init__(self, board):
+    def __init__(self, uint64_number):
         """
         Run when a BitBoard object is created.
+
+        :param numpy.uint64 uint64_number: a 64-bit unsigned integer to initialize the BitBoard to.
         """
         # a 64-bit integer that represents the bitboard
-        self.num = np.uint64(board)
+        self.num = np.uint64(uint64_number)
 
     def __getitem__(self, n):
         """
-        Allows using [] operator for a BitBoard object to retrieve nth bit from the right
-        :return: nth bit from the right
+        Allows using [] operator for a BitBoard object to retrieve nth bit from the right.
+
+        :param int n: the index of the bit to retrieve, counting from right to left.
+        :return: the bit from the right.
+        :rtype: int
         """
         return (self.num >> np.uint64(n)) & np.uint64(1)
 
     def __setitem__(self, n, bit):
         """
-        Allows using [] operator for a BitBoard object to set nth bit from the right
+        Allows using [] operator for a BitBoard object to set the nth bit from the right.
+
+        :param int n: the index of the bit to set, counting from right to left.
+        :param int bit: the value to set the nth bit to. Should be 0 or 1.
         """
         if self[n] != bit:
             self.num ^= (np.uint64(1) << np.uint64(n))
 
     def __str__(self):
         """
-        Overrides default function to a string showing a 8x8 chessboard with bits
+        Overrides default function to a string showing a 8x8 chessboard with bits.
+
         :return: a string of 64 bits
+        :rtype: str
         """
         return textwrap.fill('{:064b}'.format(self.num), 8)
 
     def __and__(self, other):
         """
-        Returns the result of bitwise and (&) of two BitBoard objects
-        :param other: a BitBoard object to AND with self
-        :return: the result of bitwise and (&) of two BitBoard objects
+        Allows using the AND operator (&) for two BitBoard objects. Returns the result of a bitwise and (&) of self
+        and other.
+
+        :param BitBoard other: a BitBoard object to AND with self
+        :return: the result of a bitwise and (&) of self and other
+        :rtype: BitBoard
         """
         return BitBoard(self.num & other.num)
 
     def __or__(self, other):
         """
-        Returns the result of bitwise or (|) of two BitBoard objects
+        Allows using the OR operator (|) for two BitBoard objects. Returns the result of a bitwise or (|) of self and
+        other.
+
         :param other: a BitBoard object to OR with self
-        :return: the result of bitwise or (|) of two BitBoard objects
+        :return: the result of a bitwise or (|) of self and other.
+        :rtype: BitBoard
         """
         return BitBoard(self.num | other.num)
 
     def __invert__(self):
         """
-        Returns the inverse of the encapsulated 64-bit integer
+        Allows using the NOT operator (~) on a BitBoard object. Returns the inverse of the encapsulated 64-bit unsigned
+        integer.
+
+        :return: the inverse of the encapsulated 64-bit unsigned integer
+        :rtype: BitBoard
         """
         return BitBoard(~self.num)
 
     def __eq__(self, other):
         """
-        TODO considering allowing other to be either Int or BitBoard type
-        Returns True if self and other have the same number, False otherwise.
+        Allows using the equality operator (==) on a BitBoard object. Returns True if self and other have the same
+        number, False otherwise.
+
+        :param BitBoard other: a BitBoard object to check equality against
         :return: True if self and other have the same number, False otherwise.
+        :rtype: bool
         """
+        # TODO considering allowing other to be either Int or BitBoard type
         return self.num == other.num
 
     def indices(self):
         """
-        Returns the indices of bits with value 1
+        Returns the indices of bits with value 1.
+
         :return: the indices of bits with value 1
+        :rtype: list of int
         """
         bits = []
         for i in range(64):
