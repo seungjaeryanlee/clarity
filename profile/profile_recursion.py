c73aafccd91e875a03e9f6ba4282e91c20557079
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-21 15:31:50 +0900

    Added simple profiling script for perft() function

---
+++ profile/profile_recursion.py
@@ -0,0 +1,13 @@
+"""
+This file profiles the code to provide analysis of most time-intensive functions in Clarity.
+Note that this file will contain PyCharm warnings on imports. Please disregard them.
+"""
+import cProfile
+
+# TODO check if there is a better method for this
+import sys
+sys.path.append('../clarity')
+from clarity.Board import Board
+from clarity.recursion import perft
+
+cProfile.run('perft(Board(), 2)')
