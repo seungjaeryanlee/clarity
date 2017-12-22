28ccd9334baeeaf6e8bbf2354c2f76f06bcfc101
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-22 09:34:32 +0900

    Added negamax to profiling

--- profile/profile_recursion.py
+++ profile/profile_recursion.py
@@ -9,6 +9,7 @@ import cProfile
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from clarity.Board import Board
-from clarity.recursion import perft
+from clarity.recursion import perft, negamax
 
 cProfile.run('perft(Board(), 2)', filename='perft.prof')
+cProfile.run('negamax(Board(), 3)', filename='negamax.prof')
