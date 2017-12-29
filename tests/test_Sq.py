2a2b434c292c88f9ddac03301701971e4b9b9159
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-29 15:51:53 +0900

    Changed unit tests of Sq enum to use pytest instead of unittest

--- tests/test_Sq.py
+++ tests/test_Sq.py
@@ -2,11 +2,10 @@
 """
 This file defines unit tests for the Sq enum.
 """
-import unittest
 from clarity.Sq import Sq
 
 
-class TestColorEnum(unittest.TestCase):
+class TestColorEnum:
     """
     This class tests the Sq enum.
     """
@@ -15,16 +14,16 @@ class TestColorEnum(unittest.TestCase):
         """
         Tests the filerank_to_sq() function of the Sq enum.
         """
-        self.assertEqual(Sq.filerank_to_sq('e1'), Sq.E1)
-        self.assertEqual(Sq.filerank_to_sq('a2'), Sq.A2)
-        self.assertEqual(Sq.filerank_to_sq('f3'), Sq.F3)
-        self.assertEqual(Sq.filerank_to_sq('d4'), Sq.D4)
+        assert Sq.filerank_to_sq('e1') == Sq.E1
+        assert Sq.filerank_to_sq('a2') == Sq.A2
+        assert Sq.filerank_to_sq('f3') == Sq.F3
+        assert Sq.filerank_to_sq('d4') == Sq.D4
 
     def test_sq_to_filerank(self):
         """
         Tests the sq_to_filerank() function of the Sq enum.
         """
-        self.assertEqual(Sq.sq_to_filerank(Sq.B5), 'b5')
-        self.assertEqual(Sq.sq_to_filerank(Sq.C6), 'c6')
-        self.assertEqual(Sq.sq_to_filerank(Sq.H7), 'h7')
-        self.assertEqual(Sq.sq_to_filerank(Sq.G8), 'g8')
+        assert Sq.sq_to_filerank(Sq.B5) == 'b5'
+        assert Sq.sq_to_filerank(Sq.C6) == 'c6'
+        assert Sq.sq_to_filerank(Sq.H7) == 'h7'
+        assert Sq.sq_to_filerank(Sq.G8) == 'g8'
