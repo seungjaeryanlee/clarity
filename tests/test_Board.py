4930a8038ff7c5a7d2a4eb9f913f1457e4b49662
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-12-31 11:10:48 +0900

    Changed unit tests of Board.__init__() to use pytest instead of unittest

--- tests/test_Board.py
+++ tests/test_Board.py
@@ -24,74 +24,65 @@ class TestBoardClass(unittest.TestCase):
         Tests the __init__() function of the Board class.
         """
         board = Board()
-        self.assertEqual(board.bitboards[Piece.WP].num,
-                         int('0000000000000000000000000000000000000000000000001111111100000000', 2))
-        self.assertEqual(board.bitboards[Piece.WN].num,
-                         int('0000000000000000000000000000000000000000000000000000000001000010', 2))
-        self.assertEqual(board.bitboards[Piece.WB].num,
-                         int('0000000000000000000000000000000000000000000000000000000000100100', 2))
-        self.assertEqual(board.bitboards[Piece.WR].num,
-                         int('0000000000000000000000000000000000000000000000000000000010000001', 2))
-        self.assertEqual(board.bitboards[Piece.WQ].num,
-                         int('0000000000000000000000000000000000000000000000000000000000010000', 2))
-        self.assertEqual(board.bitboards[Piece.WK].num,
-                         int('0000000000000000000000000000000000000000000000000000000000001000', 2))
+        assert board.bitboards[Piece.WP].num \
+            == int('0000000000000000000000000000000000000000000000001111111100000000', 2)
+        assert board.bitboards[Piece.WN].num \
+            == int('0000000000000000000000000000000000000000000000000000000001000010', 2)
+        assert board.bitboards[Piece.WB].num \
+            == int('0000000000000000000000000000000000000000000000000000000000100100', 2)
+        assert board.bitboards[Piece.WR].num \
+            == int('0000000000000000000000000000000000000000000000000000000010000001', 2)
+        assert board.bitboards[Piece.WQ].num \
+            == int('0000000000000000000000000000000000000000000000000000000000010000', 2)
+        assert board.bitboards[Piece.WK].num \
+            == int('0000000000000000000000000000000000000000000000000000000000001000', 2)
 
-        self.assertEqual(board.bitboards[Piece.BP].num,
-                         int('0000000011111111000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BN].num,
-                         int('0100001000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BB].num,
-                         int('0010010000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BR].num,
-                         int('1000000100000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BQ].num,
-                         int('0001000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BK].num,
-                         int('0000100000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.turn, Color.WHITE)
-        self.assertEqual(board.ep_square, -1)
-        self.assertEqual(board.castling[Piece.WK], True)
-        self.assertEqual(board.castling[Piece.WQ], True)
-        self.assertEqual(board.castling[Piece.BK], True)
-        self.assertEqual(board.castling[Piece.BQ], True)
-        self.assertEqual(board.half_move_clock, 0)
-        self.assertEqual(board.full_move_count, 1)
+        assert board.bitboards[Piece.BP].num \
+            == int('0000000011111111000000000000000000000000000000000000000000000000', 2)
+        assert board.bitboards[Piece.BN].num \
+            == int('0100001000000000000000000000000000000000000000000000000000000000', 2)
+        assert board.bitboards[Piece.BB].num \
+            == int('0010010000000000000000000000000000000000000000000000000000000000', 2)
+        assert board.bitboards[Piece.BR].num \
+            == int('1000000100000000000000000000000000000000000000000000000000000000', 2)
+        assert board.bitboards[Piece.BQ].num \
+            == int('0001000000000000000000000000000000000000000000000000000000000000', 2)
+        assert board.bitboards[Piece.BK].num \
+            == int('0000100000000000000000000000000000000000000000000000000000000000', 2)
+
+        assert board.turn == Color.WHITE
+        assert board.ep_square == -1
+        assert board.castling[Piece.WK]
+        assert board.castling[Piece.WQ]
+        assert board.castling[Piece.BK]
+        assert board.castling[Piece.BQ]
+        assert board.half_move_clock == 0
+        assert board.full_move_count == 1
 
         board = Board('K7/8/8/8/8/8/8/7k b - - 0 1')
-        self.assertEqual(board.bitboards[Piece.WP].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.WN].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.WB].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.WR].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.WQ].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.WK].num,
-                         int('1000000000000000000000000000000000000000000000000000000000000000', 2))
+        assert board.bitboards[Piece.WP].num == 0
+        assert board.bitboards[Piece.WN].num == 0
+        assert board.bitboards[Piece.WB].num == 0
+        assert board.bitboards[Piece.WR].num == 0
+        assert board.bitboards[Piece.WQ].num == 0
+        assert board.bitboards[Piece.WK].num \
+            == int('1000000000000000000000000000000000000000000000000000000000000000', 2)
 
-        self.assertEqual(board.bitboards[Piece.BP].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BN].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BB].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BR].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BQ].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000000', 2))
-        self.assertEqual(board.bitboards[Piece.BK].num,
-                         int('0000000000000000000000000000000000000000000000000000000000000001', 2))
-        self.assertEqual(board.turn, Color.BLACK)
-        self.assertEqual(board.ep_square, -1)
-        self.assertEqual(board.castling[Piece.WK], False)
-        self.assertEqual(board.castling[Piece.WQ], False)
-        self.assertEqual(board.castling[Piece.BK], False)
-        self.assertEqual(board.castling[Piece.BQ], False)
-        self.assertEqual(board.half_move_clock, 0)
-        self.assertEqual(board.full_move_count, 1)
+        assert board.bitboards[Piece.BP].num == 0
+        assert board.bitboards[Piece.BN].num == 0
+        assert board.bitboards[Piece.BB].num == 0
+        assert board.bitboards[Piece.BR].num == 0
+        assert board.bitboards[Piece.BQ].num == 0
+        assert board.bitboards[Piece.BK].num == int('00000001', 2)
+
+        assert board.turn == Color.BLACK
+        assert board.ep_square == -1
+        assert not board.castling[Piece.WK]
+        assert not board.castling[Piece.WQ]
+        assert not board.castling[Piece.BK]
+        assert not board.castling[Piece.BQ]
+        assert board.half_move_clock == 0
+        assert board.full_move_count == 1
 
     def test_str(self):
         """
