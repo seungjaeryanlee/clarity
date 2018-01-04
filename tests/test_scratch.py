73de7d58082ced19dcc07b0122522f78cfd2e2d0
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-01-04 14:54:58 +0900

    Added import statements to test_scratch.py for convenience

--- tests/test_scratch.py
+++ tests/test_scratch.py
@@ -4,6 +4,18 @@ This file is for debugging.
 import pytest
 
 
+from clarity.BitBoard import BitBoard
+from clarity.Board import Board
+from clarity.Color import Color
+from clarity import constants as const
+from clarity.Direction import Direction
+from clarity.Move import Move
+from clarity.MoveType import MoveType
+from clarity.Piece import Piece
+from clarity.recursion import divide, perft, negamax, _negamax_recur
+from clarity.Sq import Sq
+
+
 @pytest.mark.scratch
 def test_scratch():
     pass
