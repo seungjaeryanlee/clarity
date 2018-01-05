8ff2556e63a13de816b9b3c01464b43d54f1dc8b
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-01-05 10:51:40 +0900

    Added stub uci.py file

---
+++ clarity/uci.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This script runs Clarity Chess in UCI (Universal Chess Interface) mode.
+"""
+
+
+def uci():
+    """
+    Runs Clarity in UCI mode
+    """
+    pass
+
+
+# only runs when this module is called directly
+if __name__ == '__main__':
+    uci()
